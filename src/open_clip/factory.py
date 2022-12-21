61a08ad5d5e476b3dd9be7e2270a5398f94338cc
kmeagle1515 <kshirsagar.mrunal15@gmail.com>
2022-12-20 23:22:45 -0500

    cleaned unnecessary code

--- src/open_clip/factory.py
+++ src/open_clip/factory.py
@@ -12,7 +12,7 @@ import torch
 from torch import nn
 
 from .constants import OPENAI_DATASET_MEAN, OPENAI_DATASET_STD
-from .model import CLIP, RN50IMAGES, convert_weights_to_fp16, resize_pos_embed, SIMCLR
+from .model import CLIP, convert_weights_to_fp16, resize_pos_embed, SIMCLR
 from .openai import load_openai_model
 from .pretrained import get_pretrained_cfg, download_pretrained
 from .transform import image_transform
@@ -65,7 +65,7 @@ def _rescan_model_configs():
     for cf in config_files:
         with open(cf, 'r') as f:
             model_cfg = json.load(f)
-            if all(a in model_cfg for a in ('embed_dim', 'vision_cfg', 'text_cfg')) or (a in model_cfg for a in ('embed_dim', 'vision_cfg')):
+            if all(a in model_cfg for a in ('embed_dim', 'vision_cfg', 'text_cfg')):
                 _MODEL_CONFIGS[cf.stem] = model_cfg
 
     _MODEL_CONFIGS = {k: v for k, v in sorted(_MODEL_CONFIGS.items(), key=lambda x: _natural_key(x[0]))}
